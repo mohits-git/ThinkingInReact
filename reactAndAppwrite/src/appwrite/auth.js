a4fd7f507ca2c336e61798cae3c714a18bd1a891
mohits-git <mohitkhatri2805@gmail.com>
2024-01-08 23:38:46 +0530

    Building the mega blog project by hitesh sir

---
+++ reactAndAppwrite/src/appwrite/auth.js
@@ -0,0 +1,61 @@
+import conf from "../conf/conf";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+    client = new Client();
+    account;
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client);
+    }
+
+    async createAccount({email, password, name}){
+        try{
+            const userAccount = await this.account.create(ID.unique(), email, password )
+            if(userAccount){
+                return this.login(email, password);
+            }
+            else{
+                return userAccount;
+            }
+        }
+        catch(err) {
+            throw err;
+        }
+    }
+
+    async login({email, password}){
+        try{
+            return await this.account.createEmailSession(email, password);
+        }
+        catch(err) {
+            throw err;
+        }
+    }
+
+    async getCurrentUser(){
+        try{
+           return await this.account.get();
+        } catch(err) {
+            console.log('Appwrite service :: getCurrentUser :: error');
+        }
+        return null; 
+    }
+
+    async logout() {
+        try{
+           await this.account.deleteSessions();
+        }
+        catch(err) {
+            console.log('Appwrite service :: logout :: error');
+        }
+    }
+
+}
+
+
+const authService = new AuthService();
+
+export default authService;
